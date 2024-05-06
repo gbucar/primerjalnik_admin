aa046cfe501fb3c8e38bd5691532c3b5b292953b
Lolek <59aur0ir@duck.com>
2024-05-06 16:20:28 +0200

    fix

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -5,14 +5,8 @@ export const actions = {
         const body = Object.fromEntries(await request.formData());
         try {
             await locals.pb.admins.authWithPassword(body.username, body.password);
-            if (!locals.pb?.authStore?.model?.verified) {
-                locals.pb.authstore?.clear();
-                return {
-                    notVerified: true
-                }
-            }
         } catch (err) {
-            return { error: "" }
+            return { error: "Napaka pri prijavi! Preverite uporabniško ime in geslo. Če ste pozabili geslo ali pa se težava ponavlja mi prosim sporočite na nbmyghig@duck.com" }
         }
         throw redirect(303, "/records");
     },
