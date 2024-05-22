5e96ece3257a4b8a6e3e576f51fe4cdc67103c01
Lolek <59aur0ir@duck.com>
2024-05-22 15:42:51 +0200

    ticks lol

--- src/routes/records/+page.svelte
+++ src/routes/records/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Records from '$lib/Records.svelte';
 	import UserForm from '$lib/UserForm.svelte';
+	import Tick from './tick.svelte';
+	import Cross from './cross.svelte';
 	import { formatDate } from '$lib/utils';
 	import { slide } from 'svelte/transition';
 	// import { linear } from 'svelte/easing';
@@ -10,7 +12,7 @@
 	let filter = '';
 	let start = '';
 
-	let userShouldBeShown = (user: any, hideZero: boolean, hideInactive: boolean, filter:string) => {
+	let userShouldBeShown = (user: any, hideZero: boolean, hideInactive: boolean, filter: string) => {
 		let now = new Date();
 		let start = user.start ? new Date(user.start) : null;
 		let end = user.end ? user.end : null;
@@ -23,19 +25,20 @@
 </script>
 
 <div class="bg-white m-8 rounded-xl">
-    {#if true}
-        <button
-            class="font-bold bg-red-50"
-            on:click={() => {
-                data.users = data.users.map((user) => {
-                    return { ...user, collapsed: true };
-                });
-            }}>collapse all</button>
-    {/if}
-    show participants without records:
-    <input bind:checked={hideZero} type="checkbox" />
-    show inactive:
-    <input bind:checked={hideInactive} type="checkbox" />
+	{#if true}
+		<button
+			class="font-bold bg-red-50"
+			on:click={() => {
+				data.users = data.users.map((user) => {
+					return { ...user, collapsed: true };
+				});
+			}}>collapse all</button
+		>
+	{/if}
+	hide participants without records:
+	<input bind:checked={hideZero} type="checkbox" />
+	hide inactive:
+	<input bind:checked={hideInactive} type="checkbox" />
 	<label for="text">
 		search:
 		<input name="filter" type="text" bind:value={filter} />
@@ -45,16 +48,42 @@
 {#each data.users as user}
 	{#if userShouldBeShown(user, hideZero, hideInactive, filter)}
 		<div class="bg-white m-8 rounded-xl shadow-sm">
-			<div class="flex flex-row items-center gap-2">
+			<div class="flex flex-row items-center gap-2 justify-between">
 				<button
 					class="text-xl font-bold"
 					on:click={() => (user = { ...user, collapsed: !user.collapsed })}
 					>{user.email} ({user.records.length})</button
 				>
-				<a class="hover:bg-gray-100 rounded-full px-2" href="/records/noisecapture#{user.email}"
-					>noise</a
-				>
+				<a class="hover:bg-gray-100 rounded-full px-2" href="/records/noisecapture#{user.email}">
+					noise
+				</a>
 				<!-- <a href="/records/atmo#{user.email}">atmo</a> -->
+				<div class="flex flex-row gap-5">
+					<div class="flex flex-row items-center">
+						<p>Profile:</p>
+						{#if user.height > 5 && user.sex && user.weight > 10 && user.age > 10}
+							<Tick />
+						{:else}
+							<Cross />
+						{/if}
+					</div>
+					<div class="flex flex-row items-center">
+						<p>FitBit:</p>
+						{#if user.fitbit_fully_connected}
+							<Tick />
+						{:else}
+							<Cross />
+						{/if}
+					</div>
+					<div class="flex flex-row items-center">
+						<p>NoiseCaptureID:</p>
+						{#if user.noisecapture_id}
+							<Tick />
+						{:else}
+							<cross />
+						{/if}
+					</div>
+				</div>
 			</div>
 			{#if !user.collapsed}
 				<UserForm {user} atmotubes={data.atmotubes} />
